d36c485ce9d9da0de47fe1c4d5383c442fdcca7d
Tina Nguyen <nguyentina2005@yahoo.com>
2018-01-08 17:55:42 -0500

    php with html

--- index.php
+++ index.php
@@ -1,6 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en-us">
 
-$greeting = "Hello php";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
-    echo 'Started learning php so ' . $greeting;
-?>
+    <title>PHP practice</title>
+
+</head>
+
+<body>
+    <header>
+        <h1>
+            <?php
+            //get person name from URL parameter from the address bar
+            //the name will be in the varible
+            //use to get name if it is after url main
+            //ex localhost:8888/?name=Tina  the varible name will now print Tina in the string.
+
+                $name = htmlspecialchars($_GET['name']);
+
+                $greeting = "Hello $name";
+
+                echo 'Started learning php so, ' . $greeting;
+            ?>
+        </h1>    
+    </header>
+</body>
+
+</html>
+
+
+
+
